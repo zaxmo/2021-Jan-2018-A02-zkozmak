8a41ed271a07c5a98dcda356164186220e5e9631
zkozmak <zkozmak13@gmail.com>
2021-01-12 13:48:57 -0700

    artist entity complete

---
+++ ChinookSolution/ChinookSystem/Entities/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    class Album
+    {
+    }
+}
--- ChinookSolution/ChinookSystem/Entities/Artist.cs
+++ ChinookSolution/ChinookSystem/Entities/Artist.cs
@@ -4,10 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
 namespace ChinookSystem.Entities
 {
+    [Table("Artist")]
     internal class Artist
     {
+        private string _Name;
 
+        [Key]
+        public int ArtistId { get; set; }
+
+        [StringLength(120, ErrorMessage = "Artist name is limited to 120 characters.")]
+        public string Name 
+        { 
+            get { return _Name; } 
+            set { _Name = string.IsNullOrEmpty(value) ? null : value; }                
+        }
     }
 }
