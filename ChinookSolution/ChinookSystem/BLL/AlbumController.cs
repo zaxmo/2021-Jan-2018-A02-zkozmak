8385dbee440d00137308047a4dc4d18f03d642e4
zkozmak <zkozmak13@gmail.com>
2021-01-13 14:16:43 -0700

    controllers completed

---
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.DAL;
+using ChinookSystem.Entities;
+using ChinookSystem.ViewModels;
+using System.ComponentModel;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<ArtistAlbums> Albums_GetArtistAblums()
+        {
+            using(var context = new ChinookSystemContext())
+            {
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name
+                                                    };
+                return results.ToList();
+            }
+        }
+    }
+}
--- ChinookSolution/ChinookSystem/ViewModels/ArtistAlbums.cs
+++ ChinookSolution/ChinookSystem/ViewModels/ArtistAlbums.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ChinookSystem.ViewModels
 {
-    class dummy
+    public class ArtistAlbums
     {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public string ArtistName { get; set; }
     }
 }
