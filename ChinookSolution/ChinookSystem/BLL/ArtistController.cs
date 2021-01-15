b45c44766b74d197365bc8ed9af391bc6af7459a
zkozmak <zkozmak13@gmail.com>
2021-01-15 15:20:30 -0700

    artist controller created

---
+++ ChinookSolution/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.DAL;
+using ChinookSystem.Entities;
+using ChinookSystem.ViewModels;
+using System.ComponentModel;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class ArtistController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<SelectionList> Artists_DDLList()
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                IEnumerable<SelectionList> results = from x in context.Artists
+                                                     select new SelectionList
+                                                     {
+                                                         ValueField = x.ArtistId,
+                                                         DisplayField = x.Name
+                                                     };
+
+                return results.ToList();
+            }
+        }
+    }
+}
